a902fce9cf0280ee975563cacd313d07b3fc3630
Pierre MAGIEU <magieu.pierre@gmail.com>
2024-11-22 14:04:29 +0100

    init_game

--- src/model/model.c
+++ src/model/model.c
@@ -2,3 +2,30 @@
 #include <stdlib.h>
 
 #include <model/model.h>
+
+model *init_game(int nb_player){
+    model *game = (model *)malloc(sizeof(model));
+    if(!game){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    position *players = (position *)malloc(sizeof(position));
+    if(!players){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    game->players = players;
+
+    char *dead = (char *)calloc(nb_player, sizeof(char));
+    if(!dead){
+        perror("calloc");
+        exit(EXIT_FAILURE);
+    }
+    game->dead = dead;
+
+    game->n_player= nb_player;
+
+    // game->grid = ...
+    
+}
