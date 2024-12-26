069f11f89435da5006430ef8dc57e3824ff18f9b
Pierre MAGIEU <magieu.pierre@gmail.com>
2024-12-26 16:36:27 +0100

    du commentaire encore

--- src/views/view_sdl.h
+++ src/views/view_sdl.h
@@ -8,10 +8,25 @@
 #define HAUTEUR 600
 #define LARGEUR 900
 
+
+/**
+ * @brief Initialize the sdl view
+ * @return the view
+ */
 view *init_view_sdl();
 
-void destroy_view_sdl(view *);
+/**
+ * @brief Initialize the sdl view
+ * @param v the view we want free
+ */
+void destroy_view_sdl(view *v);
 
+/**
+ * @brief get the direction of player in the game depending on the key they push
+ * @param v the view where the direction is retrieve
+ * @param nb_player_on_keyboard the number of players to know for which player the direction corresponds
+ * @return the direction retrieved
+ */
 direction get_direction_sdl(view *v, int nb_player_on_keyboard, direction *dirs);
 
 void update_screen_sdl(view *, int, int *, int **, int , int);
